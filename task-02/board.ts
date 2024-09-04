ccab8f5d69f6cfd869000a721874edd09b79ab1c
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-05 01:00:10 +0600

    Был написан код для создания шахмотной доски в файле board.ts!

--- task-02/board.ts
+++ task-02/board.ts
@@ -1,1 +1,16 @@
-console.log("Шахматная доска!");
+let board: string = '';
+
+for (let i: number = 0; i < 8; i++) {
+    for (let j: number = 0; j < 8; j++) {
+
+        if ((i + j) % 2 === 0) {
+            board += '██';
+        } else {
+            board += '  ';
+        }
+    }
+
+    board += '\n';
+}
+
+console.log(board);
