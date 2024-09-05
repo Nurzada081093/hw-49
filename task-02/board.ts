06ea761b64ae108ce7e9b12533518b7b258f803d
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-05 13:27:48 +0600

    Были изменения в файлах board.ts и package.json. Если более детально, то в файле board.ts были изменения в содании квадратов для шахмотной доски. Что касаемо package.json был изменен запуск шахмотной доски, чтоб запустить без дополнительных команд.

--- task-02/board.ts
+++ task-02/board.ts
@@ -4,9 +4,9 @@ for (let i: number = 0; i < 8; i++) {
     for (let j: number = 0; j < 8; j++) {
 
         if ((i + j) % 2 === 0) {
-            board += '██';
+            board += '▓▓ ';
         } else {
-            board += '  ';
+            board += '░░ ';
         }
     }
 
